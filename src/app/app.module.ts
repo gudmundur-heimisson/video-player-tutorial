37cf0ec9768e93debcd7a5d6e8f64cf678a37e4c
Gudmundur Heimisson <gudmundur.heimisson@gmail.com>
2017-10-18 18:01:18 -0700

    Add video player app components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { VideoPlayerComponent } from './video-player/video-player.component';
+import { VideoPlayerGridComponent } from './video-player-grid/video-player-grid.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VideoPlayerComponent,
+    VideoPlayerGridComponent
   ],
   imports: [
     BrowserModule
